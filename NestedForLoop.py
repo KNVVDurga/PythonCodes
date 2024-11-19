9fc9fc059d70ee6aee22a8dcb70a33dfb72b0e70
KNVVDurga <43975897+KNVVDurga@users.noreply.github.com>
2024-11-19 20:12:43 +0530

    Update NestedForLoop.py

--- NestedForLoop.py
+++ NestedForLoop.py
@@ -1,3 +1,13 @@
+'''You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.'''
+
+#In Actual Leetcode problem the above are the rules, Here we tried with duplicate jewels example
+
 stones="aaamNcccvAbbB"
 jewels = "aaABAbbbb"
 total_jewels = 0
@@ -5,7 +15,7 @@ for i in stones:
     for j in jewels:
         if (i==j):
             total_jewels+=1
-            break
+            break #Avoids incrementing the var again as there are duplicates in jewels
 return(total_jewels)
 
 ######OR#####
